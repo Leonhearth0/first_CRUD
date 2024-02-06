12c95407228dbe415eff6b23af34eac50981581e
Leonhearth0 <valery.johann@gmail.com>
2024-02-06 12:47:36 +0100

    add regex on update

--- index.php
+++ index.php
@@ -5,74 +5,89 @@ $password = '';
 $dbname = 'projet_crud';
 
 try {
-$db = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-if ($_SERVER["REQUEST_METHOD"] == "POST" && !isset($_GET['id'])) {
-    $nom = $_POST['nom'];
-    $prenom = $_POST['prenom'];
-    $mail = $_POST['mail'];
-    $codePostal = $_POST['codePostal'];
-
-
-    if (isset($_POST['supprUser'])) {
-        deleteUser($db, $_POST['supprUser']);
-        header('Location: index.php');
-    }
-
-    if (empty($nom) || !preg_match("/^[a-zA-Z-']*$/", $nom)) {
-        $_SESSION['error']['nom'] = "Merci de remplir le champs 'Nom' avec des lettres et des tirets uniquement.";
-    }
-    if (empty($prenom) || !preg_match("/^[a-zA-Z-']*$/", $prenom)) {
-        $_SESSION['error']['prenom'] = "Merci de remplir le champs 'Prénom' avec des lettres et des tirets uniquement.";
-    }
-    if (empty($mail) || !preg_match("/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/", $mail)) {
-        $_SESSION['error']['mail'] = "Merci de remplir le champs 'Mail' avec une adresse email valide";
-    }
-    if (empty($codePostal) || !preg_match("/^[0-9]{5}$/", $codePostal)) {
-        $_SESSION['error']['codePostal'] = "Merci de remplir le champs 'Code Postal' avec un code postal à 5 chiffres";
-    }
-
-    if (empty($_SESSION['error'])) {
-        $sql = "INSERT INTO utilisateurs (Nom, Prenom, Mail, CodePostal) VALUES (?, ?, ?, ?)";
-        $statement = $db->prepare($sql);
-        $statement->execute([$nom, $prenom, $mail, $codePostal]);
-
-        header("Location: index.php");
-        exit;
-    }
-}
-
-if (isset($_GET['id'])) {
-    $id = $_GET['id'];
-
-    $sql = "SELECT * FROM utilisateurs WHERE ID = ?";
-    $statement = $db->prepare($sql);
-    $statement->execute([$id]);
-
-    $utilisateur = $statement->fetch(PDO::FETCH_ASSOC);
-
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && !isset($_GET['id'])) {
         $nom = $_POST['nom'];
         $prenom = $_POST['prenom'];
         $mail = $_POST['mail'];
         $codePostal = $_POST['codePostal'];
 
-        $sql = "UPDATE utilisateurs SET Nom = ?, Prenom = ?, Mail = ?, CodePostal = ? WHERE ID = ?";
-        $statement = $db->prepare($sql);
-        $statement->execute([$nom, $prenom, $mail, $codePostal, $id]);
 
-        header("Location: index.php");
-        exit;
+        if (isset($_POST['supprUser'])) {
+            deleteUser($db, $_POST['supprUser']);
+            header('Location: index.php');
+        }
+
+        if (empty($nom) || !preg_match("/^[a-zA-Z-']*$/", $nom)) {
+            $_SESSION['error']['nom'] = "Merci de remplir le champs 'Nom' avec des lettres et des tirets uniquement.";
+        }
+        if (empty($prenom) || !preg_match("/^[a-zA-Z-']*$/", $prenom)) {
+            $_SESSION['error']['prenom'] = "Merci de remplir le champs 'Prénom' avec des lettres et des tirets uniquement.";
+        }
+        if (empty($mail) || !preg_match("/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/", $mail)) {
+            $_SESSION['error']['mail'] = "Merci de remplir le champs 'Mail' avec une adresse email valide";
+        }
+        if (empty($codePostal) || !preg_match("/^[0-9]{5}$/", $codePostal)) {
+            $_SESSION['error']['codePostal'] = "Merci de remplir le champs 'Code Postal' avec un code postal à 5 chiffres";
+        }
+
+        if (empty($_SESSION['error'])) {
+            $sql = "INSERT INTO utilisateurs (Nom, Prenom, Mail, CodePostal) VALUES (?, ?, ?, ?)";
+            $statement = $db->prepare($sql);
+            $statement->execute([$nom, $prenom, $mail, $codePostal]);
+
+            header("Location: index.php");
+            exit;
+        }
     }
-}
 
-$sql = "SELECT * FROM utilisateurs";
-$statement = $db->prepare($sql);
-$statement->execute();
+    if (isset($_GET['id'])) {
+        $id = $_GET['id'];
 
-$utilisateurs = $statement->fetchAll(PDO::FETCH_ASSOC);}
- catch (PDOException $e) {
+        $sql = "SELECT * FROM utilisateurs WHERE ID = ?";
+        $statement = $db->prepare($sql);
+        $statement->execute([$id]);
+
+        $utilisateur = $statement->fetch(PDO::FETCH_ASSOC);
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $nom = $_POST['nom'];
+            $prenom = $_POST['prenom'];
+            $mail = $_POST['mail'];
+            $codePostal = $_POST['codePostal'];
+
+            if (empty($nom) || !preg_match("/^[a-zA-Z-']*$/", $nom)) {
+                $_SESSION['error']['nom'] = "Merci de remplir le champs 'Nom' avec des lettres et des tirets uniquement.";
+            }
+            if (empty($prenom) || !preg_match("/^[a-zA-Z-']*$/", $prenom)) {
+                $_SESSION['error']['prenom'] = "Merci de remplir le champs 'Prénom' avec des lettres et des tirets uniquement.";
+            }
+            if (empty($mail) || !preg_match("/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/", $mail)) {
+                $_SESSION['error']['mail'] = "Merci de remplir le champs 'Mail' avec une adresse email valide";
+            }
+            if (empty($codePostal) || !preg_match("/^[0-9]{5}$/", $codePostal)) {
+                $_SESSION['error']['codePostal'] = "Merci de remplir le champs 'Code Postal' avec un code postal à 5 chiffres";
+            }
+
+            if (empty($_SESSION['error'])) {
+                $sql = "UPDATE utilisateurs SET Nom = ?, Prenom = ?, Mail = ?, CodePostal = ? WHERE ID = ?";
+                $statement = $db->prepare($sql);
+                $statement->execute([$nom, $prenom, $mail, $codePostal, $id]);
+
+                header("Location: index.php");
+                exit;
+            }
+        }
+    }
+
+    $sql = "SELECT * FROM utilisateurs";
+    $statement = $db->prepare($sql);
+    $statement->execute();
+
+    $utilisateurs = $statement->fetchAll(PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
     die("Erreur : " . $e->getMessage());
 }
 ?>
@@ -101,25 +116,25 @@ $utilisateurs = $statement->fetchAll(PDO::FETCH_ASSOC);}
             <?php if (isset($_SESSION['error']['nom']))
                 echo $_SESSION['error']['nom']; ?>
         </label><br>
-        <input type="text" id="nom" name="nom" placeholder="Entrez le nom de l'utilisateur"><br>
+        <input type="text" id="nom" name="nom" placeholder="Entrez le nom de l'utilisateur."><br>
 
         <label for="prenom">Prénom:
             <?php if (isset($_SESSION['error']['prenom']))
                 echo $_SESSION['error']['prenom']; ?>
         </label><br>
-        <input type="text" id="prenom" name="prenom" placeholder="Entrez le prénom de l'utilisateur"><br>
+        <input type="text" id="prenom" name="prenom" placeholder="Entrez le prénom de l'utilisateur."><br>
 
         <label for="mail">Mail:
             <?php if (isset($_SESSION['error']['mail']))
                 echo $_SESSION['error']['mail']; ?>
         </label><br>
-        <input type="text" id="mail" name="mail" placeholder="Entrez l'adresse mail de l'utilisateur"><br>
+        <input type="text" id="mail" name="mail" placeholder="Entrez l'adresse mail de l'utilisateur."><br>
 
         <label for="codePostal">Code Postal:
             <?php if (isset($_SESSION['error']['codePostal']))
                 echo $_SESSION['error']['codePostal']; ?>
         </label><br>
-        <input type="text" id="codePostal" name="codePostal" placeholder="Entrez le code postal de l'utilisateur"><br>
+        <input type="text" id="codePostal" name="codePostal" placeholder="Entrez le code postal de l'utilisateur."><br>
 
         <input type="submit" id="adduserButton" value="Ajouter utilisateur">
     </form>
@@ -154,13 +169,13 @@ $utilisateurs = $statement->fetchAll(PDO::FETCH_ASSOC);}
                     <?= $utilisateur['CodePostal'] ?>
                 </td>
                 <td>
-                <div class="button-container">
-                    <a href="?id=<?= $utilisateur['ID'] ?>" class="modifierButton">Modifier</a>
-                    <form method="POST" action="index.php" class="deluserform">
-                        <input type="hidden" name="supprUser" value="<?= $utilisateur['ID'] ?>">
-                        <input type="submit" class="supprButton" value="Supprimer">
-                    </form>
-        </div>
+                    <div class="button-container">
+                        <a href="?id=<?= $utilisateur['ID'] ?>" class="modifierButton">Modifier</a>
+                        <form method="POST" action="index.php" class="deluserform">
+                            <input type="hidden" name="supprUser" value="<?= $utilisateur['ID'] ?>">
+                            <input type="submit" class="supprButton" value="Supprimer">
+                        </form>
+                    </div>
                 </td>
             </tr>
             <?php
@@ -172,9 +187,10 @@ $utilisateurs = $statement->fetchAll(PDO::FETCH_ASSOC);}
 
 </html>
 
-<?php function deleteUser($db, $id) {
-        $sqlQuery = "DELETE FROM utilisateurs WHERE ID = :id";
-        $statement = $db->prepare($sqlQuery);
-        $statement->bindParam(':id', $id);
-        $statement->execute();
-    } ?>
+<?php function deleteUser($db, $id)
+{
+    $sqlQuery = "DELETE FROM utilisateurs WHERE ID = :id";
+    $statement = $db->prepare($sqlQuery);
+    $statement->bindParam(':id', $id);
+    $statement->execute();
+} ?>
